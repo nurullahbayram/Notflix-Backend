ac6550c1b6c896a322c4a40aff8a7cdf7dcc4318
Khaled7042 <eneskarabey24@gmail.com>
2022-09-07 12:51:17 +0200

    paramter edited

--- src/main/java/com/notflix/notflix/controller/MovieController.java
+++ src/main/java/com/notflix/notflix/controller/MovieController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @CrossOrigin("*")
@@ -15,25 +16,29 @@ public class MovieController {
     @Autowired
     private MovieServiceImpl movieServiceImpl;
 
-        //http://localhost:8090/api/movie/all
+    //http://localhost:8090/api/movie/all
     @GetMapping("/all")
     @ResponseBody
-    public List<Movie> getMovies(@RequestParam(required = false) String id){
-        //http://localhost:8090/api/movie/all?id=
-        if(id!=null){
-           return movieServiceImpl.getMovies().stream().filter(x->x.getId()==Integer.valueOf(id)).toList();
-        }
-            return movieServiceImpl.getMovies();
+    public List<Movie> getMovies() {return movieServiceImpl.getMovies();}
+
+    //http://localhost:8090/api/movie/all/{id}
+    @GetMapping(value = {"/all/{id}"})
+    @ResponseBody
+    public Movie getEmployeesByIdWithRequired(@PathVariable int id) {
+        List<Movie> movies = movieServiceImpl.getMovies().stream().filter(x -> x.getId() == id).toList();
+        return movies.get(0);
     }
 
+
     @PostMapping("/add")
-    public Movie createMovie(@RequestBody Movie movie){
+    public Movie createMovie(@RequestBody Movie movie) {
         return movieServiceImpl.createMovie(movie);
     }
 
     @PutMapping()
-    public Movie updateMovie(@RequestBody Movie movie){
+    public Movie updateMovie(@RequestBody Movie movie) {
         return movieServiceImpl.updateMovie(movie);
     }
-
 }
+
+
